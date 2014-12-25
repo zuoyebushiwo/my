159c7662910b9cf3a4ac8e5e0fff633f011ceda1
zhangxuej <957168943@qq.com>
2014-12-25 14:57:27 +0800

    13131

--- src/com/zuoye/pattern/adapter/Client.java
+++ src/com/zuoye/pattern/adapter/Client.java
@@ -4,6 +4,9 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		AbstractComputerPower computerPower = new ComputerPowerAdapter();
+		System.out.println(computerPower.provideDirectCurrent());
+		
 	}
 	
 }
