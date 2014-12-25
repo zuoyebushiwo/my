5121173698a9cb1f9f28b2bf3c6f1343ae543e87
zhangxuej <957168943@qq.com>
2014-12-25 11:45:47 +0800

    Signed-off-by: zhangxuej <957168943@qq.com>

---
+++ src/com/zhangxuej/base/createobject/Worker.java
@@ -0,0 +1,43 @@
+package com.zhangxuej.base.createobject;
+
+import java.io.Serializable;
+
+/**
+ * @author zhangxuej
+ */
+public class Worker implements Cloneable, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	private int age;
+	
+	public Worker() {
+		this.name = "";
+		this.age = 0;
+	}
+	
+	public Worker(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public void work() {
+		System.out.println(name + "is working");
+	}
+	
+	@Override
+	protected Object clone() {
+		Worker worker = null;
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return worker;
+	}
+	
+}
