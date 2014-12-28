0a64d300c87cd736ae871155d4ba8b58a920f60a
zhangxuej <957168943@qq.com>
2014-12-28 16:21:30 +0800

    Change-Id: I1d355c39ebac58df4dd814e63577fccdc5fdf4ab

--- src/com/zuoye/base/hashmap/HashMapTest.java
+++ src/com/zuoye/base/hashmap/HashMapTest.java
@@ -29,9 +29,18 @@ public class HashMapTest {
 	 * 会根据Key的hashCode值，以某种映射关系，决定应当将这对键值对Entry<Key, Value>存储在HashMap中的什么位置；
 	 * 
 	 * 		当通过Key值取数据的时候，然后根据Key值的hashcode，以及内部映射条件，直接定位到Key对应的Value值存放在什么位置上，
-	 * 可以非常搞笑地将Value值取出。
+	 * 可以非常高效地将Value值取出。
 	 */
 
+	
+	/**
+	 * 为了实现上述的设计思路，在HashMap内部，采用了数组+链表的形式来组织键值对Entry<Key, Value>。
+	 * HashMap内部维护了一个Entry[] table数组，当我们使用new HashMap()创建一个HashMap时，Entry[] table的默认长度为16。Entry[] table的长度又被称
+	 * 为这个HashMap的容量（capacity）；
+	 * 对于Entry[] table的每个元素而言，或为null，或为由若干个Entry<Key, Value>组成的链表。HashMap中的Entry<Key, Value>的数目被称为HashMap的大小
+	 * （size）;
+	 * Entry[] table中的某一个元素及其对应Entry<Key, Value>又被称为桶(bucket);
+	 */
 	public static void main(String[] args) {
 		HashMap<String, String> map = new HashMap<String, String>();
 	}
