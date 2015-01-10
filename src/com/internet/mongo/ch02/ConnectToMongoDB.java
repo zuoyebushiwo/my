c5178973a3c2c130282365275d64d386021cdfee
zhangxuej <957168943@qq.com>
2015-01-10 17:09:16 +0800

    Change-Id: I613dff8e51fc93ce27b782a566ff798737a6b41b

--- src/com/internet/mongo/ch02/ConnectToMongoDB.java
+++ src/com/internet/mongo/ch02/ConnectToMongoDB.java
@@ -1,5 +1,57 @@
 package com.internet.mongo.ch02;
 
+import java.net.UnknownHostException;
+import java.util.Set;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
 public class ConnectToMongoDB {
 
+	Mongo m = null;
+	DB db;
+
+	public void connect() {
+		try {
+			m = new Mongo("localhost", 27017);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void listAllCollections(String dbName) {
+		if (m != null) {
+			db = m.getDB(dbName);
+			Set<String> collections = db.getCollectionNames();
+			for (String s : collections) {
+				System.out.println(s);
+			}
+		}
+	}
+
+	public void listLocationCollectionDocuments() {
+		if (m != null) {
+			db = m.getDB("prefs");
+			DBCollection collection = db.getCollection("location");
+			DBCursor cur = collection.find();
+			while (cur.hasNext()) {
+				System.out.println(cur.next());
+			}
+		} else {
+			System.out.println("Please connect to MongoDB and then fetch the collection");
+		}
+	}
+	
+	public static void main(String[] args) {
+		ConnectToMongoDB connectToMongoDB = new ConnectToMongoDB();
+		connectToMongoDB.connect();
+		connectToMongoDB.listAllCollections("prefs");
+		connectToMongoDB.listLocationCollectionDocuments();
+	}
+	
 }
