1c8f09a77b8f66629f36cedc3bf514873b5fc2a6
zhangxuej <957168943@qq.com>
2015-01-14 22:08:47 +0800

    Change-Id: I138aa1a67b450bbf5d2103958e2c5e5bb913fb5b

---
+++ src/com/zuoye/thread/nomal/MyThread.java
@@ -0,0 +1,33 @@
+package com.zuoye.thread.nomal;
+
+public class MyThread extends Thread {
+	private int id = 0;
+	private Common common;
+
+	public MyThread(String name, int no, Common object) {
+		super(name);
+		common = object;
+		id = no;
+	}
+
+	public void run() {
+		System.out.println("Running Thread" + this.getName());
+		try {
+			if (id == 0) {
+				common.synchronizedMethod1();
+			} else {
+				common.synchronizedMethod2();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Common c = new Common();
+		MyThread t1 = new MyThread("MyThread-1", 0, c);
+		MyThread t2 = new MyThread("MyThread-2", 1, c);
+		t1.start();
+		t2.start();
+	}
+}
