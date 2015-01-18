a8fe9541048978e7db45dd630bf5e1da0c661fc0
zhangxuej <957168943@qq.com>
2015-01-18 11:23:41 +0800

    Change-Id: Ib5d291ef6a57aba35eb00bc345280f21bfbe5a2f

--- src/com/thread/hight/Consumer.java
+++ src/com/thread/hight/Consumer.java
@@ -1,5 +1,22 @@
 package com.thread.hight;
 
+/**
+ * 消费者
+ */
 public class Consumer extends Thread {
+	
+	private int neednum; // 消费产品的数量
+	private Godown godown; // 仓库
+	
+	public Consumer(int neednum, Godown godown) {
+		this.neednum = neednum;
+		this.godown = godown;
+	}
+	
+	@Override
+	public void run() {
+		// 消费指定数量的产品
+		godown.consume(neednum);
+	}
 
 }
