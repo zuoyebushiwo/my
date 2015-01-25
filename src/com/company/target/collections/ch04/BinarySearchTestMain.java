584ed124e60e5d8a1c1c409b4baf476467354546
zhangxuej <957168943@qq.com>
2015-01-25 22:08:06 +0800

    Change-Id: I32305553061abb7084b5334d6df655766df27e15

---
+++ src/com/company/target/collections/ch04/BinarySearchTestMain.java
@@ -0,0 +1,12 @@
+package com.company.target.collections.ch04;
+
+public class BinarySearchTestMain {
+
+    public static void main (String args[]) {
+
+       BinarySearchTest test = new BinarySearchTest();
+
+    } // method main
+
+} // class BinarySearchTestMain
+
