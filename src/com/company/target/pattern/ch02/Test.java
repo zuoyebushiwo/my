688bbcbfe474e2d6db0389fda362be055cc33209
zhangxuej <957168943@qq.com>
2015-01-29 13:43:18 +0800

    Change-Id: I534a7a89235eab29604bf1947a20e782f13b9488

--- src/com/company/target/pattern/ch02/Test.java
+++ src/com/company/target/pattern/ch02/Test.java
@@ -1,12 +1,19 @@
 package com.company.target.pattern.ch02;
 
+import java.beans.PropertyChangeListener;
 import java.util.Observable;
 import java.util.Observer;
+
+import javax.swing.AbstractButton;
 
 public class Test {
 
 	Observer observer;
 	
 	Observable observable;
+	
+	PropertyChangeListener propertyChangeListener;
+	
+	AbstractButton abstractButton;
 	
 }
