fbe12c3021135fb02177e36faccf999f57e64ac1
zhangxuej <957168943@qq.com>
2015-01-29 11:50:38 +0800

    Change-Id: I2ea728a0aa0afbbab569a5546d90af861ebe3435

---
+++ src/com/company/target/pattern/ch02/Test.java
@@ -0,0 +1,12 @@
+package com.company.target.pattern.ch02;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Test {
+
+	Observer observer;
+	
+	Observable observable;
+	
+}
---
+++ src/com/company/target/pattern/ch02/WeatherStationObservable/WeatherData.java
@@ -0,0 +1,5 @@
+package com.company.target.pattern.ch02.WeatherStationObservable;
+
+public class WeatherData {
+
+}
