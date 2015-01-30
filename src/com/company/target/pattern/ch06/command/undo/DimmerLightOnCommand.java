b6eec971a25fd8f2a3131dcea8fd8c7774736230
zhangxuej <957168943@qq.com>
2015-01-30 15:58:27 +0800

    Change-Id: I6dbd780c8b0c920c4689ee944062a293e9be438c

---
+++ src/com/company/target/pattern/ch06/command/undo/DimmerLightOnCommand.java
@@ -0,0 +1,19 @@
+package com.company.target.pattern.ch06.command.undo;
+
+public class DimmerLightOnCommand implements Command {
+	Light light;
+	int prevLevel;
+
+	public DimmerLightOnCommand(Light light) {
+		this.light = light;
+	}
+
+	public void execute() {
+		prevLevel = light.getLevel();
+		light.dim(75);
+	}
+
+	public void undo() {
+		light.dim(prevLevel);
+	}
+}
