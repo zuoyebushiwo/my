526c79bf2b91d6b1ddb219bbea7b5bfde1d9921b
zhangxuej <957168943@qq.com>
2015-01-30 15:35:44 +0800

    Change-Id: I7dd6e86cfa98bf6a14fa87d93f115b522cc5e457

--- src/com/company/target/pattern/ch06/command/simpleremote/RemoteControlTest.java
+++ src/com/company/target/pattern/ch06/command/simpleremote/RemoteControlTest.java
@@ -1,18 +1,17 @@
 package com.company.target.pattern.ch06.command.simpleremote;
 
 public class RemoteControlTest {
-	
+
 	public static void main(String[] args) {
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light light = new Light();
+		GarageDoor garageDoor = new GarageDoor();
 		LightOnCommand lightOn = new LightOnCommand(light);
-		
+		GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
+
 		remote.setCommand(lightOn);
 		remote.buttonWasPressed();
-		
-		GarageDoor garageDoor = new GarageDoor();
-		GarageDoorOpenCommand garageDoorOpen = new GarageDoorOpenCommand(garageDoor);
-		remote.setCommand(garageDoorOpen);
+		remote.setCommand(garageOpen);
 		remote.buttonWasPressed();
 	}
 
