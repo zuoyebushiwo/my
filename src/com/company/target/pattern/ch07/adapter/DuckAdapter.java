20b0c56a6060327750bc8ddf16b5d6ee0c5ab629
zhangxuej <957168943@qq.com>
2015-01-30 16:34:42 +0800

    Change-Id: If2c7cf09855e87553b6fde392ecdc0e24a9c8389

--- src/com/company/target/pattern/ch07/adapter/DuckAdapter.java
+++ src/com/company/target/pattern/ch07/adapter/DuckAdapter.java
@@ -1,5 +1,25 @@
 package com.company.target.pattern.ch07.adapter;
 
-public class DuckAdapter {
+import java.util.Random;
 
+public class DuckAdapter implements Turkey {
+
+	Duck duck;
+	Random rand;
+
+	public DuckAdapter(Duck duck) {
+		this.duck = duck;
+	}
+
+	@Override
+	public void gobble() {
+		duck.quack();
+	}
+
+	@Override
+	public void fly() {
+		if (rand.nextInt(5) == 0) {
+			duck.fly();
+		}
+	}
 }
--- src/com/company/target/pattern/ch07/adapter/TurkeyTestDrive.java
+++ src/com/company/target/pattern/ch07/adapter/TurkeyTestDrive.java
@@ -1,14 +1,14 @@
 package com.company.target.pattern.ch07.adapter;
 
 public class TurkeyTestDrive {
-
 	public static void main(String[] args) {
-		
-		Duck duck = new MallardDuck();
-		Turkey turkey = new WildTurkey();
-		
-		
-		
+		MallardDuck duck = new MallardDuck();
+		Turkey duckAdapter = new DuckAdapter(duck);
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("The DuckAdapter says...");
+			duckAdapter.gobble();
+			duckAdapter.fly();
+		}
 	}
-	
 }
