096fc71b14fe9b492830f9718780260052542894
zhangxuej <957168943@qq.com>
2015-01-30 11:41:05 +0800

    Change-Id: Id25dff62e4a3dc8712151da9a2320149454d7be7

---
+++ src/com/company/target/pattern/ch04/factory/pizzaaf/VeggiePizza.java
@@ -0,0 +1,17 @@
+package com.company.target.pattern.ch04.factory.pizzaaf;
+
+public class VeggiePizza extends Pizza {
+	PizzaIngredientFactory ingredientFactory;
+ 
+	public VeggiePizza(PizzaIngredientFactory ingredientFactory) {
+		this.ingredientFactory = ingredientFactory;
+	}
+ 
+	void prepare() {
+		System.out.println("Preparing " + name);
+		dough = ingredientFactory.createDough();
+		sauce = ingredientFactory.createSauce();
+		cheese = ingredientFactory.createCheese();
+		veggies = ingredientFactory.createVeggies();
+	}
+}
