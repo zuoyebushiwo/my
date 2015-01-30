bf9d57eeed54b0f92ee8702af1b9ae8683d6428a
zhangxuej <957168943@qq.com>
2015-01-30 15:57:25 +0800

    Change-Id: I31603b1e8ed77e16cba78607ecc5598c8d363a02

--- src/com/company/target/pattern/ch06/command/undo/NoCommand.java
+++ src/com/company/target/pattern/ch06/command/undo/NoCommand.java
@@ -1,6 +1,12 @@
 package com.company.target.pattern.ch06.command.undo;
 
 public class NoCommand implements Command {
-	public void execute() { }
-	public void undo() { }
+	
+	public void execute() {
+		
+	}
+
+	public void undo() {
+		
+	}
 }
