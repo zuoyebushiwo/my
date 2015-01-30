56b5419e9434e64be5aecd0f484ab9c2c4d4a7a5
zhangxuej <957168943@qq.com>
2015-01-30 10:33:31 +0800

    Change-Id: Id6d859c9a84416880cd15ef3676bf5acacf36447

---
+++ src/com/company/target/pattern/ch04/factory/pizzaaf/Dough.java
@@ -0,0 +1,11 @@
+package com.company.target.pattern.ch04.factory.pizzaaf;
+
+/**
+ * 接口：面团
+ */
+public interface Dough {
+
+	@Override
+	public String toString();
+	
+}
---
+++ src/com/company/target/pattern/ch04/factory/pizzaaf/PizzaIngredientFactory.java
@@ -0,0 +1,10 @@
+package com.company.target.pattern.ch04.factory.pizzaaf;
+
+/**
+ * 接口：负责创建所有的原料
+ */
+public interface PizzaIngredientFactory {
+
+	
+	
+}
