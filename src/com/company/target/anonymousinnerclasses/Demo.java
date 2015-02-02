12da6e90f19c0b4ed07b7d94a61ca80959ea990d
zhangxuej <957168943@qq.com>
2015-02-02 09:51:37 +0800

    Change-Id: I0d426e0b009a74486c0952022eab489d7edf314f

--- src/com/company/target/anonymousinnerclasses/Demo.java
+++ src/com/company/target/anonymousinnerclasses/Demo.java
@@ -7,7 +7,7 @@ package com.company.target.anonymousinnerclasses;
  * 		正因为没有名字，所以匿名内部类只能使用一次，它通常用于简化代码编写
  * 		但使用匿名内部类还有个前提条件：必须继承一个父类或实现一个接口
  */
-public class Demo {
+public class Demo { 
 
 	public static void main(String[] args) {
 //		Person p = new Child();
