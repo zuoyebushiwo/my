3194c3e5e9e27fb05743d2d41bf0cbfab8acec06
zhangxuej <957168943@qq.com>
2015-02-02 11:46:43 +0800

    Change-Id: I0d43f0e1e823a38f1177d1b1634ccc11f90940fc

---
+++ src/com/company/target/pattern/ch09/composite/Waitress.java
@@ -0,0 +1,15 @@
+package com.company.target.pattern.ch09.composite;
+
+public class Waitress {
+
+	MenuComponent allMenus;
+
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+
+	public void printMenu() {
+		allMenus.print();
+	}
+
+}
