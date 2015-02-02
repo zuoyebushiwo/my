557dc0fd5abd53de61d3e379436eec91e6b65b25
zhangxuej <957168943@qq.com>
2015-02-02 08:59:19 +0800

    Change-Id: I96da282d5ea26b12dd53dafc18307bbaf918e46e

--- src/com/company/target/collections/ch05/Test.java
+++ src/com/company/target/collections/ch05/Test.java
@@ -2,6 +2,7 @@ package com.company.target.collections.ch05;
 
 import java.util.AbstractCollection;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Test {
