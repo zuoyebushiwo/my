dda86e1673ba9511d9a2f142a5dcd75b89b82cf3
zhangxuej <957168943@qq.com>
2015-04-20 23:24:53 +0800

    Change-Id: Ib91f7e7b3e1015e027d947815102ae3d11fa003e

---
+++ src/com/zuoye/nio/chapter04/MyTest.java
@@ -0,0 +1,11 @@
+package com.zuoye.nio.chapter04;
+
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+
+public class MyTest {
+	
+	Selector selector;
+	SelectionKey selectionKey;
+	
+}
