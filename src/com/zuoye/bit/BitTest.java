3c11d0e2199dbddb8ccf468840968752f10aa958
zuoyebushiwo <957168943@qq.com>
2015-10-27 19:01:19 +0800

    111

---
+++ src/com/zuoye/bit/BitTest.java
@@ -0,0 +1,13 @@
+package com.zuoye.bit;
+
+/**
+ * @author ZuoYe
+ * @date 2015年10月27日
+ */
+public class BitTest {
+
+    public static void main(String[] args) {
+        System.out.println( 1 << 10);
+    }
+
+}
