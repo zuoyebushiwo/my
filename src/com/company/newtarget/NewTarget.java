5b07e385d89a0d74dfb608ff81d846266f80d9d4
zuoyebushiwo <957168943@qq.com>
2016-03-15 14:16:34 +0800

    111

---
+++ src/com/company/newtarget/NewTarget.java
@@ -0,0 +1,24 @@
+package com.company.newtarget;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * @date 2016年03月15日
+ */
+public class NewTarget {
+
+    private volatile  boolean falg;
+
+    public static void main(String[] args) {
+
+        Executor executor;
+
+        ExecutorService executorService;
+
+        ScheduledExecutorService scheduledExecutorService;
+
+    }
+
+}
---
+++ src/com/company/one/Test.java
@@ -0,0 +1,115 @@
+package com.company.one;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test extends TestCase {
+	
+	public List<File> getFileListByPath(String path) {
+		File folder = new File(path);
+		List<File> rtn = new LinkedList<File>();
+		for(File temp : folder.listFiles()) {
+			rtn.add(temp);
+			if (temp.isDirectory()) {
+				rtn.addAll(getFileListByPath(temp.getAbsolutePath()));
+			} 
+		}
+		return rtn;
+	}
+	
+	public void testGetFileListByPath() {
+//		List<File> files = getFileListByPath("D:\\pp\\ipes_upload");
+//		for(File file : files) {
+//			System.out.println(file.getName());
+//		}
+	}
+	
+	int i = 0;
+	
+	class AddThread implements Runnable {
+
+		
+		@Override
+		public void run() {
+			add();
+		}
+
+		private synchronized void add() {
+			++i;
+		}
+		
+	}
+	
+	class SubThread extends Thread {
+		
+		
+		@Override
+		public void run() {
+			sub();
+		}
+
+		private void sub() {
+			i--;
+		}
+		
+	}
+
+	
+	public void testThread() {
+		Thread add1 = new Thread(new AddThread());
+		Thread add2 = new Thread(new AddThread());
+		
+		Thread sub1 = new SubThread();
+		Thread sub2 = new SubThread();
+		
+		add1.start();
+		add2.start();
+		
+		sub1.start();
+		sub2.start();
+		
+		try {
+//			add1.join();
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(i);
+	}
+	
+	public void printGrap() {
+		String a = "o";
+		System.out.println(a);
+		for(int i = 0; i < 4; i++) {
+			a += "oo";
+			System.out.println(a);
+		}
+	}
+	
+	public void testPrintGrap() {
+		printGrap();
+	}
+	
+	public void pattern(String mail) {
+		Pattern p = Pattern.compile("\\p{Alnum}+@\\w+\\.\\p{Alpha}{2,3}");
+		Matcher m = p.matcher(mail);
+		if (m.find()) {
+			System.out.println(m.group());
+		}
+	}
+	
+	public void testPattern() {
+		pattern("junxuezhang@yeah.net");
+	}
+	
+	static void main(String[] args) {
+		//int i = 078;
+	}
+	
+}
